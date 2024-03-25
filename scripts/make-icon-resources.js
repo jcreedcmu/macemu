5e59ef208f3c2cbf9ea25d134f900a19e787e98f
Jason Reed <jcreed@gmail.com>
2024-03-25 09:32:09 -0400

    Start adding small icons

--- scripts/make-icon-resources.js
+++ scripts/make-icon-resources.js
@@ -1,45 +1,92 @@
 const fs = require('fs');
 const palette = require('./palette');
 
-const toks = fs.readFileSync('/tmp/twelf-icon.ppm', 'utf8')
-		.split('\n')
-		.map(x => x.replace(/#.*/, ''))
-		.filter(x => x.match(/\S/) && !x.match("#"))
-		.join(' ').split(/\s+/)
-		.filter(x => x.match(/\S/));
-toks.splice(0,4); // P3 W H C
-const nums = toks.map(x => parseInt(x));
+function iconsOfPpm(ppmFile) {
+  const toks = fs.readFileSync('/tmp/twelf-icon.ppm', 'utf8')
+		  .split('\n')
+		  .map(x => x.replace(/#.*/, ''))
+		  .filter(x => x.match(/\S/) && !x.match("#"))
+		  .join(' ').split(/\s+/)
+		  .filter(x => x.match(/\S/));
+  const width = toks[1];
+  const height = toks[2];
+  toks.splice(0,4); // P3 W H C
+  const nums = toks.map(x => parseInt(x));
 
-const img = [];
-const mask = [];
+  const img = [];
+  const mask = [];
+  const ic8Bytes = [];
+  const ic4Nybs = [];
+  const ic1Bits = [];
+  const expandMask = [];
 
-for (let x = 0; x < 32; x++) {
-  for (let y = 0; y < 32; y++) {
-	 const ix = y * 32 + x;
-	 img[ix] = [nums[ix * 3], nums[ix * 3+1], nums[ix*3+2]];
+  for (let x = 0; x < width; x++) {
+	 for (let y = 0; y < height; y++) {
+		const ix = y * width + x;
+		img[ix] = [nums[ix * 3], nums[ix * 3+1], nums[ix*3+2]];
+	 }
   }
+
+  // cheating a bit with the color choices to match the icon as I've drawn it:
+  palette.clut4[8] = [17*6, 17*9, 17*6];
+  palette.clut4[9] = [11, 49, 10];
+
+  img.forEach((pixel, ix) => {
+	 // interpret cyan as transparent
+	 if (pixel[0] == 0 && pixel[1] == 255 && pixel[2] == 255) {
+		ic8Bytes.push(0);
+		ic4Nybs.push(0);
+		mask[ix] = 0;
+	 }
+	 else {
+		ic8Bytes.push(palette.getIndex(pixel, palette.clut8));
+		ic4Nybs.push(palette.getIndex(pixel, palette.clut4));
+		mask[ix] = 1;
+	 }
+	 ic1Bits.push((pixel[0] + pixel[1] + pixel[2] > 3*128) ? 0 : 1);
+  });
+
+  function pixelAt(x, y) {
+	 return (x >= 0 && x < width && y >= 0 && y < height) ? mask[width * y + x] : 0;
+  }
+
+  // expand mask by one pixel all around
+  for (let x = 0; x < width; x++) {
+	 for (let y = 0; y < height; y++) {
+		expandMask[width * y + x] = (pixelAt(x, y) || pixelAt(x+1,y) || pixelAt(x,y+1) || pixelAt(x-1,y) || pixelAt(x,y-1)) ? 1 : 0;
+	 }
+  }
+
+  return {
+	 pixels:img, width, height, mask: expandMask, ic8Bytes, ic4Nybs, ic1Bits
+  };
 }
 
-const icl8Bytes = [];
-img.forEach((pixel, ix) => {
-  // interpret cyan as transparent
-  if (pixel[0] == 0 && pixel[1] == 255 && pixel[2] == 255) {
-	 icl8Bytes.push(0);
-	 mask[ix] = 0;
-  }
-  else {
-	 icl8Bytes.push(palette.getIndex(pixel));
-	 mask[ix] = 1;
-  }
-});
+const largeIcon = iconsOfPpm('/tmp/twelf-icon.ppm');
+const smallIcon = iconsOfPpm('/tmp/twelf-icon-small.ppm');
 
-const maskBytes = [];
-for (let i = 0; i < mask.length / 8; i++) {
-  let b = 0;
-  for (let j = 0; j < 8; j++) {
-	 b = 2 * b + mask[i * 8 + j];
+function bytesOfBits(bits) {
+  const bytes = [];
+  for (let i = 0; i < bits.length / 8; i++) {
+	 let b = 0;
+	 for (let j = 0; j < 8; j++) {
+		b = 2 * b + bits[i * 8 + j];
+	 }
+	 bytes.push(b);
   }
-  maskBytes.push(b);
+  return bytes;
+}
+
+function bytesOfNybbles(nybbles) {
+  const bytes = [];
+  for (let i = 0; i < nybbles.length / 2; i++) {
+	 let b = 0;
+	 for (let j = 0; j < 2; j++) {
+		b = 16 * b + nybbles[i * 2 + j];
+	 }
+	 bytes.push(b);
+  }
+  return bytes;
 }
 
 function hexOfByte(x) {
@@ -73,12 +120,17 @@ resource 'FREF' (128) {
 console.log(`
 resource 'ICN#' (128) {
   {
-    ${rezOfBytes(maskBytes)},
-    ${rezOfBytes(maskBytes)},
+    ${rezOfBytes(bytesOfBits(largeIcon.ic1Bits))},
+    ${rezOfBytes(bytesOfBits(largeIcon.mask))},
   }
 };
 
 resource 'icl8' (128) {
-  ${rezOfBytes(icl8Bytes)}
+  ${rezOfBytes(largeIcon.ic8Bytes)}
 };
+
+resource 'icl4' (128) {
+  ${rezOfBytes(bytesOfNybbles(largeIcon.ic4Nybs))}
+};
+
 `);
--- scripts/palette.js
+++ scripts/palette.js
@@ -1,7 +1,28 @@
-// Code derived from this pseudocode:
+// 4-bit palette extracted from
+// https://en.wikipedia.org/wiki/List_of_software_palettes#Apple_Macintosh_default_16-color_palette
+const clut4 = [
+  [255, 255, 255],
+  [252, 244, 0],
+  [255, 100, 0],
+  [221, 2, 2],
+  [241, 2, 133],
+  [70, 0, 166],
+  [0, 0, 213],
+  [0, 174, 233],
+  [26, 185, 12],
+  [0, 100, 8],
+  [88, 40, 0],
+  [145, 113, 53],
+  [193, 193, 193],
+  [129, 129, 129],
+  [62, 62, 62],
+  [0, 0, 0],
+];
+
+// 8-bit palette generation code derived from this pseudocode:
 // http://belkadan.com/blog/2018/01/Color-Palette-8/
 
-function clutColor(x) { // x a byte value
+function clut8Color(x) { // x a byte value
   if (x < 215) {
     // Component-based colors, with RGB values in the range 0...5,
     // stored in reverse order (i.e. color #0 is white, (1.0, 1.0, 1.0)).
@@ -33,16 +54,16 @@ function clutColor(x) { // x a byte value
   }
 }
 
-function getClut() {
+function getClut8() {
   const clut = [];
   for (let i = 0; i < 256; i++) {
-	 const c = clutColor(i);
+	 const c = clut8Color(i);
 	 clut[i] = [c.red, c.green, c.blue].map(x => Math.floor(255*x));
   }
   return clut;
 }
 
-const clut = getClut();
+const clut8 = getClut8();
 
 function sq(x) { return x*x; }
 
@@ -50,10 +71,10 @@ function compare(c1, c2) {
   return sq(c1[0]-c2[0]) + sq(c1[1]-c2[1]) + sq(c1[2]-c2[2]);
 }
 
-function getIndex(color) {
+function getIndex(color, clut) {
   let bestScore = 1e16;
   let bestIx = -1;
-  for (let i = 0; i < 256; i++) {
+  for (let i = 0; i < clut.length; i++) {
 	 const score = compare(color, clut[i]);
 	 if (score < bestScore) {
 		bestScore = score;
@@ -64,3 +85,5 @@ function getIndex(color) {
 }
 
 exports.getIndex = getIndex;
+exports.clut4 = clut4;
+exports.clut8 = clut8;
