e13d2fbc23f781846c37e3581c9cd2c4466c9e3e
Jason Reed <jcreed@gmail.com>
2024-04-02 18:04:52 -0400

    Progress towards mlton-m68k compiling

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
-    Retro68.url = "github:autc04/Retro68";
+    Retro68.url = "github:agoode/Retro68/mlton";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -14,6 +14,36 @@
       in
         {
           packages = rec {
+            mlton-m68k = pkgs.stdenv.mkDerivation {
+              name = "mlton-m68k";
+              src = builtins.fetchGit {
+                url = "https://github.com/agoode/mlton.git";
+                rev = "8401144962437491018343888a3eaa20b1b8fe37";
+                ref = "mac";
+              };
+
+              buildInputs = [
+                # Retro68.packages.${system}.standalone
+                pkgs.mlton
+                pkgs.gmp
+              ];
+
+              patchPhase = ''
+              patchShebangs bin/host-arch
+              patchShebangs bin/host-os
+              patchShebangs bin/platform
+              patchShebangs mlnlffigen/gen-cppcmd
+              '';
+
+              buildPhase = ''
+              make all
+              '';
+
+              installPhase = ''
+              make PREFIX=$out install
+              '';
+            };
+
             twelf-bin = pkgs.stdenv.mkDerivation rec {
               name = "twelf-bin";
               src = ./.;
