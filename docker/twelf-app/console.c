1ba5ba244b9f40969bff7f45c00ea83226f0f04e
Jason Reed <jcreed@gmail.com>
2024-04-12 12:34:32 -0400

    Make stderr go to debug window
    Fix #27

--- docker/twelf-app/console.c
+++ docker/twelf-app/console.c
@@ -4,19 +4,41 @@
 #include <string.h>
 #include <sys/types.h>
 
+#include "document.h"
+#include "global-state.h"
+
 /***
  * We need to define *both* _consolewrite and _consoleread to somehow
  * override the ones in default libRetroConsole.a, I don't understand
  * why yet.
  */
 
-TEHandle outputDest = NULL;
+TEHandle stdoutDest = NULL;
 
-void setOutputDest(TEHandle output) { outputDest = output; }
+void setOutputDest(TEHandle output) { stdoutDest = output; }
 
-TEHandle getOutputDest() { return outputDest; }
+TEHandle getOutputDest() { return stdoutDest; }
 
 extern ssize_t _consolewrite(int fd, const void *buf, size_t count) {
+  // stdoutDest is where our caller would prefer stdout to appear, if
+  // it is non-NULL. The reason we let the caller decide where stdout
+  // is redirected is keeping the possibility open of having several
+  // output windows, one for each input window.
+  TEHandle outputDest = stdoutDest;
+
+  // Treat stderr specially. Since we might have debugging log
+  // messages arising from circumstances that are not really
+  // "per-input-file", we just lump them all together into the global
+  // debug log window. In any event, stderr should never go to the
+  // output window.
+  if (fd == 2) {
+    if (gLogWindow != NULL) {
+      outputDest = getDoc(gLogWindow)->docTE;
+    } else {
+      outputDest = NULL;
+    }
+  }
+
   if (outputDest != NULL) {
     char *tmpBuf = malloc(count);
     strncpy(tmpBuf, (char *)buf, count);
